e878fed7c1472ec90251aa684de03f6a903c27f4
SandipanNayek <sandipannayek09@gmail.com>
2025-10-22 20:16:05 +0530

    Update project2.js

--- project_2.js/project_2/project2.js
+++ project_2.js/project_2/project2.js
@@ -1,26 +1,38 @@
-const form=document.querySelector('form')
+const form = document.querySelector('form')
 
-form.addEventListener('submit',function(e){
+form.addEventListener('submit', function(e) {
     e.preventDefault()
 
-    const height=parseInt(document.querySelector('#height').value)
-    const weight=parseInt(document.querySelector('#weight').value)
-    const results=document.querySelector('#results')
-    const results1=document.querySelector('#results1')
-    if(height==='' || height<0 || isNaN(height)){
-    results.innerHTML=`please give a valid height ${height}`;
+    const heightValue = document.querySelector('#height').value
+    const weightValue = document.querySelector('#weight').value
+    const results = document.querySelector('#results')
+    const results1 = document.querySelector('#results1')
+
+    
+    if (heightValue === '' || heightValue < 0 || isNaN(heightValue) || !/^\d+$/.test(heightValue)) {
+        results.innerHTML = `please give a valid height ${heightValue}`;
     }
 
-    else if(weight==='' || weight<0 || isNaN(weight)){
-    results.innerHTML=`please give a valid weight ${weight}`;
+    
+    else if (weightValue === '' || weightValue < 0 || isNaN(weightValue) || !/^\d+$/.test(weightValue)) {
+        results.innerHTML = `please give a valid weight ${weightValue}`;
     }
-    else{
-       const bmi= (weight / ((height*height) /10000)).toFixed(2)
-        results.innerHTML=`<span>${bmi}</span>`;
-       if(bmi < 18.6) {
-         results1.innerHTML="Under Weight";
-    }
-       else if(bmi>=18.6 && bmi<=24.9) results1.innerHTML="Normal Range";
-       else results1.innerHTML="Overweight";
+
+   
+    else {
+        const height = parseInt(heightValue)
+        const weight = parseInt(weightValue)
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2)
+        results.innerHTML = `<span>${bmi}</span>`;
+
+        if (bmi < 18.6) {
+            results1.innerHTML = "Under Weight";
+        }
+        else if (bmi >= 18.6 && bmi <= 24.9) {
+            results1.innerHTML = "Normal Range";
+        }
+        else {
+            results1.innerHTML = "Overweight";
+        }
     }
 })
